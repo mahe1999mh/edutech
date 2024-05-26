a376bea778ed7ea66511273278013ef3b5c6953c
mahe1999mh <mahe1999mh@gmail.com>
2024-05-27 00:10:50 +0530

    changes

--- api/controllers/userControllers.js
+++ api/controllers/userControllers.js
@@ -23,9 +23,9 @@ const allUsers = asyncHandler(async (req, res) => {
 //@route           POST /api/user/
 //@access          Public
 const registerUser = asyncHandler(async (req, res) => {
-  const { name, email, password, pic } = req.body;
+  const { email, password, ...rest } = req.body;
 
-  if (!name || !email || !password) {
+  if ( !email || !password) {
     res.status(400);
     throw new Error("Please Enter all the Feilds");
   }
@@ -38,20 +38,19 @@ const registerUser = asyncHandler(async (req, res) => {
   }
 
   const user = await User.create({
-    name,
+
     email,
     password,
-    pic,
+   ...rest
   });
 
   if (user) {
     res.status(201).json({
       _id: user._id,
-      name: user.name,
       email: user.email,
       isAdmin: user.isAdmin,
-      pic: user.pic,
       token: generateToken(user._id),
+      ...rest
     });
   } else {
     res.status(400);
