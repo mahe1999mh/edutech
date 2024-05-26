b234dae9468769025f95e882674f2772f4553a97
mahe1999mh <mahe1999mh@gmail.com>
2024-05-26 23:36:34 +0530

    as

--- server2/index.js
+++ server2/index.js
@@ -1,37 +1,14 @@
+// Import packages
 const express = require("express");
-const userRoutes = require("./routes/userRoutes");
-const cors = require("cors");
-const connectDB = require("./config/db");
-const { authUser } = require("./controllers/userControllers");
+const home = require("./routes/home");
 
-require("dotenv").config();
-
-connectDB();
+// Middlewares
 const app = express();
 app.use(express.json());
 
-// Define CORS options
-const corsOptions = {
-  origin: "*",
-  credentials: true,
-  allowedHeaders: [
-    "Content-Type",
-    "Authorization",
-    "X-Requested-With",
-    "Origin",
-    "Accept",
-  ],
-  methods: "GET, HEAD, PUT, PATCH, POST, DELETE", // Allowed HTTP methods
-};
+// Routes
+app.use("/home", home);
 
-// Enable CORS with specific options
-app.use(cors(corsOptions));
-
-const port = process.env.PORT;
-
-app.use("/api/user", userRoutes);
-app.use("/api/user/login", authUser);
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+// connection
+const port = process.env.PORT || 9001;
+app.listen(port, () => console.log(`Listening to port ${port}`));
