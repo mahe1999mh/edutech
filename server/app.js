cf7a59a4ecf69bd85595bb483038249bb37a398b
mahe1999mh <mahe1999mh@gmail.com>
2024-05-26 23:11:10 +0530

    changes

--- server/app.js
+++ server/app.js
@@ -1,12 +1,11 @@
 const express = require("express");
-const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken");
 const cors = require("cors");
 const connectDB = require("./config/db");
 const bodyParser = require("body-parser");
-const User = require("./models/userLoginModel");
 const app = express();
+const userRoutes = require("./routes/auth")
 require("dotenv").config();
+
 
 //middleware
 connectDB();
@@ -15,55 +14,9 @@ app.use(cors());
 
 //env variables
 const port = process.env.PORT;
-const JWT_SECRET = process.env.JWT_SECRET;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
 
-app.post("/api/user/login", async (req, res) => {
-  const { email, password } = req.body;
-
-  try {
-    const user = await User.findOne({ email });
-    if (!user) {
-      return res.status(400).json({ message: "Invalid credentials" });
-    }
-    const isMatch = await bcrypt.compare(password, user.password);
-    if (!isMatch) {
-      return res.status(400).json({ message: "Invalid credentials" });
-    }
-    // Generate a JWT token
-    const token = jwt.sign({ id: user._id }, JWT_SECRET, { expiresIn: "1h" });
-    res.json({ token });
-  } catch (error) {
-    console.error("Error during login:", error);
-    res.status(500).json({ message: "Server error" });
-  }
-});
-
-app.post("/api/user/signup", async (req, res) => {
-  const { email, password, firstName, lastName } = req.body;
-
-  try {
-    // Check if the email already exists
-    let user = await User.findOne({ email });
-    if (user) {
-      return res.status(400).json({ message: "Email already exists" });
-    }
-
-    // Create a new user
-    const newUser = new User({ email, password, firstName, lastName });
-
-    // Save the user to the database
-    await newUser.save();
-
-    res.status(201).json({ message: "User created successfully" });
-  } catch (error) {
-    console.error("Error during signup:", error);
-    res.status(500).json({ message: "Server error" });
-  }
-});
+app.use(userRoutes);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
---
+++ server/routes/auth.js
@@ -0,0 +1,53 @@
+const express = require("express");
+const User = require("../models/userLoginModel");
+const router = express.Router();
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+const JWT_SECRET = process.env.JWT_SECRET;
+
+router.post("/api/user/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+    // Generate a JWT token
+    const token = jwt.sign({ id: user._id }, JWT_SECRET, { expiresIn: "1h" });
+    res.json({ token });
+  } catch (error) {
+    console.error("Error during login:", error);
+    res.status(500).json({ message: "Server error" });
+  }
+});
+
+router.post("/api/user/signup", async (req, res) => {
+  const { email, password, firstName, lastName } = req.body;
+
+  try {
+    // Check if the email already exists
+    let user = await User.findOne({ email });
+    if (user) {
+      return res.status(400).json({ message: "Email already exists" });
+    }
+
+    // Create a new user
+    const newUser = new User({ email, password, firstName, lastName });
+
+    // Save the user to the database
+    await newUser.save();
+
+    res.status(201).json({ message: "User created successfully" });
+  } catch (error) {
+    console.error("Error during signup:", error);
+    res.status(500).json({ message: "Server error" });
+  }
+});
+
+module.exports = router;
